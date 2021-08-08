09825a96ad74842db65c8e0c8c7fd1124643998f
Igna Garcia <gnachoxp@gmail.com>
2021-08-08 20:27:32 -0300

    Init project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/index.css';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    hola mundo en react
   </React.StrictMode>,
   document.getElementById('root')
 );
